81be002508aaa1c14d236756d8bd6c8b32f13ad0
Dustin Schau <dustinschau@gmail.com>
2017-05-01 16:56:45 -0500

    chore: change git hooks

--- presentation/index.js
+++ presentation/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 import {
   Appear,
@@ -18,41 +18,41 @@ import {
   S as Span,
   Slide,
   Text
-} from "spectacle";
+} from 'spectacle';
 
-import "prismjs";
-import "prismjs/components/prism-typescript";
+import 'prismjs';
+import 'prismjs/components/prism-typescript';
 
-import CodeSlide from "spectacle-code-slide";
+import CodeSlide from 'spectacle-code-slide';
 
 // Import image preloader util
-import preloader from "spectacle/lib/utils/preloader";
+import preloader from 'spectacle/lib/utils/preloader';
 
 // Import theme
-import createTheme from "spectacle/lib/themes/default";
+import createTheme from 'spectacle/lib/themes/default';
 
 // Require CSS
-require("normalize.css");
-require("spectacle/lib/themes/default/index.css");
+require('normalize.css');
+require('spectacle/lib/themes/default/index.css');
 
 const images = {
-  disgusted: "https://media.giphy.com/media/JvmMnCS1z4VQk/giphy.gif",
-  appreciated: "https://media.giphy.com/media/xSM46ernAUN3y/giphy.gif",
-  indifferent: "https://media.giphy.com/media/l0K4nCoSjvMx0agYE/giphy.gif"
+  disgusted: 'https://media.giphy.com/media/JvmMnCS1z4VQk/giphy.gif',
+  appreciated: 'https://media.giphy.com/media/xSM46ernAUN3y/giphy.gif',
+  indifferent: 'https://media.giphy.com/media/l0K4nCoSjvMx0agYE/giphy.gif'
 };
 
 preloader(images);
 
 const theme = createTheme(
   {
-    primary: "white",
-    secondary: "#1F2022",
-    tertiary: "#03A9FC",
-    quartenary: "#CECECE"
+    primary: 'white',
+    secondary: '#1F2022',
+    tertiary: '#03A9FC',
+    quartenary: '#CECECE'
   },
   {
-    primary: "Montserrat",
-    secondary: "Helvetica"
+    primary: 'Montserrat',
+    secondary: 'Helvetica'
   }
 );
 
@@ -61,7 +61,7 @@ export default class Presentation extends React.Component {
     return (
       <Deck
         progress="bar"
-        transition={["slide"]}
+        transition={['slide']}
         transitionDuration={500}
         theme={theme}
       >
@@ -69,9 +69,9 @@ export default class Presentation extends React.Component {
           <Heading size={1} fit>Angular</Heading>
           <Heading size={6} fit textColor="primary">
             Note: Angular refers to the
-            {" "}
+            {' '}
             <Span type="italic">latest</Span>
-            {" "}
+            {' '}
             version of Angular. AngularJS refers to v1.x
           </Heading>
         </Slide>
@@ -149,9 +149,9 @@ export default class Presentation extends React.Component {
             <Appear>
               <ListItem>
                 Quite a heavy framework (file size
-                {" "}
+                {' '}
                 <Span type="italic">and</Span>
-                {" "}
+                {' '}
                 functionality)
               </ListItem>
             </Appear>
@@ -178,33 +178,33 @@ export default class Presentation extends React.Component {
         <CodeSlide
           transition={[]}
           lang="typescript"
-          code={require("raw-loader!../assets/code/component.test.ts")}
+          code={require('raw-loader!../assets/code/component.test.ts')}
           ranges={[
-            { loc: [0, 27], title: "Testing an angular component" },
+            { loc: [0, 27], title: 'Testing an angular component' },
             { loc: [0, 1] },
             { loc: [8, 18] },
             { loc: [18, 25] },
-            { loc: [0, 27], title: "😢" }
+            { loc: [0, 27], title: '😢' }
           ]}
         />
         <CodeSlide
           transition={[]}
           lang="typescript"
-          code={require("raw-loader!../assets/code/component.vue.test.ts")}
+          code={require('raw-loader!../assets/code/component.vue.test.ts')}
           ranges={[
-            { loc: [0, 18], title: "Testing a vue component" },
+            { loc: [0, 18], title: 'Testing a vue component' },
             { loc: [0, 1] },
             { loc: [7, 10] },
             { loc: [11, 16] },
-            { loc: [0, 18], title: "👍" }
+            { loc: [0, 18], title: '👍' }
           ]}
         />
         <CodeSlide
           transition={[]}
           lang="typescript"
-          code={require("raw-loader!../assets/code/component.ts")}
+          code={require('raw-loader!../assets/code/component.ts')}
           ranges={[
-            { loc: [0, 12], title: "A sample Angular component" },
+            { loc: [0, 12], title: 'A sample Angular component' },
             { loc: [0, 1] },
             { loc: [2, 7] },
             { loc: [8, 10] },
@@ -214,13 +214,13 @@ export default class Presentation extends React.Component {
         <CodeSlide
           transition={[]}
           lang="html"
-          code={require("raw-loader!../assets/code/component.vue")}
+          code={require('raw-loader!../assets/code/component.vue')}
           ranges={[
-            { loc: [0, 16], title: "A sample vue component" },
+            { loc: [0, 16], title: 'A sample vue component' },
             { loc: [0, 3] },
             { loc: [4, 8] },
             { loc: [10, 15] },
-            { loc: [0, 16], title: "😊" }
+            { loc: [0, 16], title: '😊' }
           ]}
         />
         <Slide bgColor="secondary" textColor="tertiary">
